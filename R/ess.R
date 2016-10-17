1c395cedcd72c4874983dafe4dca4e948d33ecc1
Jouni Helske <jouni.helske@jyu.fi>
2016-10-17 20:43:29 +0100

    plot

--- R/ess.R
+++ R/ess.R
@@ -1,6 +1,6 @@
 #' Effective sample size
 #'
-#' Computes the effective sample size (ESS) of importance sampling estimator
+#' Computes the effective sample size (ESS) of importance sampling estimator.
 #' 
 #' @export
 #' @param w A numeric vector of non-negative weights.
@@ -15,5 +15,26 @@ ess <- function(w, f, x){
     res <- abs(f(x)) * w
     res <- res / sum(res)
     1 / sum(res ^ 2)
+  }
+}
+#' Running effective sample size
+#'
+#' Computes and returns the running estimate of effective sample size (ESS) of 
+#' importance sampling estimator.
+#' 
+#' @export
+#' @param w A numeric vector of non-negative weights.
+#' @param f A function used in computing \code{f}-specific ESS.
+#' @param x A numeric vector of samples used to generate \code{w}. Used for computing \code{f(x)}.
+#' @return An effective sample size estimate.
+running_ess <- function(w, f, x){
+  if (missing(f) || missing(x)) {
+    csw <- cumsum(w)
+    sapply(seq_along(w), function(i) 1/sum((w[1:i]/csw[i])^2))
+  } else {
+    if (!is.function(f)) stop("Argument 'f' must be a function")
+    res <- abs(f(x)) * w
+    csw <- cumsum(res)
+    sapply(seq_along(res), function(i) 1/sum((res[1:i]/csw[i])^2))
   }
 }
---
+++ R/weight_plot.R
@@ -0,0 +1,35 @@
+#' Diagnostic plot of importance sampling weights
+#'
+#' Function \code{weight_plot} plots four figures given the weight vector \code{w}: 
+#' Plot of largest weights, sorted graph of all weights, running variance estimate of weights, 
+#' and running effective sample size estimate of weights.
+#' 
+#' @importFrom ggplot2 ggplot geom_point ggtitle scale_y_continuous
+#' @importFrom gridExtra grid.arrange
+#' @param w Vector of weights
+#' @export
+#' @examples
+#' #' importance sampling from too narrow distribution
+#' #' weights have infinite variance
+#' set.seed(1)
+#' x_inf <- rnorm(1000, sd = 0.1)
+#' w_inf <- dnorm(x_inf) / dnorm(x_inf, 0, 0.1)
+#' weight_plot(w_inf)
+#' x_opt <- rnorm(1000, sd = sqrt(2))
+#' w_opt <- dnorm(x_opt) / dnorm(x_opt, 0, sqrt(2))
+#' weight_plot(w_opt)
+weight_plot <- function(w){
+  ind <- w > sort(w, decreasing = TRUE)[min(100, length(w))]
+  dat <- data.frame(weight = w[ind], index = which(ind))
+  p1 <- ggplot(dat, aes(x = index, y = weight)) + geom_point() + 
+    ggtitle(paste0(min(100, length(w)), " largest weights"))
+  index <- seq_along(w)
+  p2 <- ggplot(mapping = aes(x = index, y = sort(w))) + geom_point() + 
+    scale_y_continuous("variance") + 
+    ggtitle("sorted weights")
+  p3 <- ggplot(mappin = aes(x = index, y = running_var(w))) + geom_line() + 
+    scale_y_continuous("value") + ggtitle("running variance of weights")
+  p4 <- ggplot(mapping = aes(x = index, y = running_ess(w))) + geom_line() + 
+    scale_y_continuous("value") + ggtitle("running ESS")
+  grid.arrange(p1, p2, p3, p4, ncol=2)
+}
